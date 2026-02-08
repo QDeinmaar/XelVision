7e7c803cfe2fa696539013afbc54d26d9b6c63c1
Youness Ben <tudorbenhadda@gmail.com>
2026-02-08 18:53:59 +0100

    Working on Phase 1

--- headers.h
+++ headers.h
@@ -3,5 +3,7 @@
 #include <windows.h>
 #include <tlhelp32.h>
 #include <iostream>
+#include <stdio.h>
+#include <tchar.h>
 
 using namespace std;
--- source.cpp
+++ source.cpp
@@ -1,3 +1,13 @@
-#include <headers.h>
+#include "headers.h"
 
-get
+BOOL ProcessList()
+{
+    HANDLE hProcessSnap;
+
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if(hProcessSnap == INVALID_HANDLE_VALUE) //
+    {
+        
+    }
+}
