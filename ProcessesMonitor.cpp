2502003ff8ab4908354465ce2485d1f6651d53d5
Youness Ben <tudorbenhadda@gmail.com>
2026-02-20 00:28:10 +0000

    Working on Phase 2

--- ProcessesMonitor.cpp
+++ ProcessesMonitor.cpp
@@ -1,20 +1,29 @@
 #include "headers.h"
 
-HANDLE hProcessSnap;
 set<DWORD> seenPIDs;
 
 BOOL DetectNewProcess (set<DWORD> &seenPIDs)
 {
-    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     PROCESSENTRY32 pe32;
     pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    if( hProcessSnap == INVALID_HANDLE_VALUE)
+    {
+        return FALSE;
+    }
     if(Process32First(hProcessSnap, &pe32))
     {
         do
     {
+        DWORD pid = pe32.th32ProcessID;
 
+        if(seenPIDs.find(pid) == seenPIDs.end())
+        {
+            seenPIDs.insert(pid);
+        }
     } 
     while (Process32Next(hProcessSnap, &pe32));
     }
-    
+    CloseHandle(hProcessSnap);
 }
